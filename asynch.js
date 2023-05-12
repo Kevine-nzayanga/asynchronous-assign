cbb4ce6b6516c3a68e483f9fb3a53a649baea7eb
student <kevinenzayanga@gmail.com>
2023-05-12 18:42:17 +0300

    question

--- asynch.js
+++ asynch.js
@@ -10,15 +10,34 @@ setTimeout(entry,5000,"Time to shine")
 // that returns a Promise with user data when given a user ID. Write
 //  an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
 
- const userIds=[3,5,1,8]
 
- async function getUserData() {
-    for (const id of userIds) {
-      const userData = await getUserData(id);
+
+  const userIds = [234, 987, 453, 222, 349];
+async function fetchDetails() {
+  for (const id of userIds) {
+    try {
+      const userData = await getUserDetails(id);
       console.log(userData);
+    } catch (error) {
+      console.log(`Error fetching user id`);
     }
   }
-  
+}
+function getUserDetails(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+       
+      if (id >= 1 && id <= userIds.length)
+      {
+        resolve({ id, data: "id is present" });
+      } else {
+        reject(`invalid id number: ${id}`);
+      }
+    }, 2000);
+  });
+}
+fetchDetails()
+getUserDetails
 
 // You have an asynchronous function performTask() that returns a Promise. The Promise resolves
 //  if the task is successful and rejects if there's an error. Write a function that calls performTask() 
